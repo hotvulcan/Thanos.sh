7c9c8d2fbe198b3bf7d9655cb28eb2e2edfbb253
Mingye Wang <arthur200126@gmail.com>
2018-08-20 16:06:57 +0800

    oops squash this commit (shuf -z)

--- Thanos.sh
+++ Thanos.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-let i=`find -type f . | wc -l`/2 ; find -type f -print0 . | shuf -n $i | xargs -0 -- rm -f
+let i=`find -type f . | wc -l`/2 ; find -type f -print0 . | shuf -z -n $i | xargs -0 -- rm -f
