faac956041e5672e51021da47b81327d9663631a
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2019-04-16 09:35:55 +0800

    Add explaination

--- Thanos.sh
+++ Thanos.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 let i=`find . -type f | wc -l`/2 ; find . -type f -print0 | shuf -z -n $i | xargs -0 -- cat 
+
+# Explaination
+## Step 1: Get the count of files in current path divided by two.
+## Step 2: Get all the files in current path and print in one line.
+## Step 3: Turn half of the second step output into standard input randomly.
+## Step 4: Show half of the files in terminal.
+
+# Key Point
+## If you want to make delete, what you need to do is turn 'cat' into 'rm'.
