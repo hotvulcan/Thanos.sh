6084768ea316f1d26d0b4de79ac5610607f91874
changdingfang <245292011@qq.com>
2021-02-25 07:41:52 +0800

    增加 <响指·练习模式>

--- Thanos.sh
+++ Thanos.sh
@@ -1,15 +1,44 @@
 #!/bin/sh
-let "i=`find . -type f | wc -l`/2";
-if [[ uname=="Darwin" ]]; then
-    find . -not -name "Thanos.sh" -type f -print0 | gshuf -z -n $i | xargs -0  -- cat;
-else
-    find . -not -name "Thanos.sh" -type f -print0 | shuf -z -n $i | xargs -0  -- cat;
-fi
+
+filename=${0##*/}
+rm_dir="./temp"
+
+function touch_file()
+{
+    if [[ ! -d ${rm_dir} ]]; then
+        mkdir -p ${rm_dir}
+    fi
+
+    cnt=10
+    while [[ cnt -gt 0 ]];
+    do
+        touch ${rm_dir}/${cnt}.txt
+        let cnt-=1
+    done
+}
+
+function rm_files()
+{
+    sf="shuf"
+    if [[ "`uname`" == "Darwin" ]]; then
+        sf="gshuf"
+    fi
+
+    files=()
+    while read line;
+    do
+        files+=("${line}")
+    done <<< "`find ${rm_dir} -not -name "${filename}" -type f`"
+
+    let nums=${#files[*]}/2
+    echo ${files[*]} | xargs printf "%s\0" | ${sf} -z -n ${nums} | xargs -0 -- sudo rm -f
+    echo "nums: ${nums}"
+}
+
+touch_file
+rm_files
+
 # Explanation
 ## Step 1: Get the count of files in current path divided by two.
 ## Step 2: Get all the files in current path and print in one line.
 ## Step 3: Turn half of the second step output into standard input randomly.
-## Step 4: Show half of the files in terminal.
-
-# Key Point
-## If you want to make delete, what you need to do is turn 'cat' into 'rm'.
